b13e8f853439057a47b3621089c4d2d829ad88e8
shivamurali <pandiham@gmail.com>
2022-12-05 15:30:50 -0500

    Shiva's Changes

--- client/src/components/article/Article.js
+++ client/src/components/article/Article.js
@@ -1,7 +1,7 @@
-import React from 'react'
 import {Navbar, Brand, CTA} from '..';
 import {Footer, Blog, Possibility, Features, WhatMCK4, Header, ArticleBody} from '../../containers'
-
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 
 const Article = () => {
--- client/src/containers/articleBody/ArticleBody.jsx
+++ client/src/containers/articleBody/ArticleBody.jsx
@@ -10,6 +10,62 @@ import Col from 'react-bootstrap/Col';
 
 
 const ArticleBody = () => {
+    /*
+    const [newsData, setNewsData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+
+    //Make api call to news api
+    async function getNewsData() {
+    //Set loading boolean to true so that we know to show loading text
+    setLoading(true);
+
+    //Make news api call using axios
+    let topic = 'dog'
+    let articleCount = '1'
+    let url = 'https://newsapi.org/v2/everything?q='+topic+'&apiKey=b738ed2669c54125aae96fba7c1107d5&pageSize=' +articleCount
+    const resp = await axios.get(url);
+    setNewsData(resp.data.articles);
+
+    //Set loading boolean to false so that we know to show news articles
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getNewsData();
+  }, []);
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {loading ? "Loading..." : <Container>
+
+          {newsData.map((newsData, index) =>
+            <Row className="d-flex justify-content-center">
+              <Col xs={12} className="mt-5 w-500" key={index}>
+                <a target="_blank" href={newsData.url}>
+                  <Card >
+                    <Card.Title className="my-3">  {newsData.title}</Card.Title>
+                    <Card.Img src={newsData.urlToImage} />
+                    <Card.Body>
+                      <Card.Text>
+                        {newsData.description}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </a>
+              </Col>
+            </Row>
+          )}
+
+        </Container>
+        }
+      </header>
+    </div>
+  );
+  */
 }
 
 export default ArticleBody
